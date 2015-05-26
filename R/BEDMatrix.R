56f2e23990b2e04aefd597800ef1914053892414
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-05-26 16:28:40 -0500

    Add dimnames and dimnames<- method.

--- R/BEDMatrix.R
+++ R/BEDMatrix.R
@@ -3,13 +3,14 @@
 NULL
 
 #' @export
-setClass('BEDMatrix', slots = list(path = 'character', n = 'integer', p = 'integer'))
+setClass('BEDMatrix', slots = list(path = 'character', n = 'integer', p = 'integer', dimnames = 'list'))
 
 #' @export
 setMethod('initialize', signature(.Object = 'BEDMatrix'), function (.Object, path, n, p) {
   .Object@path <- path
   .Object@n <- as.integer(n)
   .Object@p <- as.integer(p)
+  .Object@dimnames <- list(NULL, NULL)
   return(.Object)
 })
 
@@ -28,4 +29,27 @@ setMethod('[', signature(x = 'BEDMatrix'), function (x, i, j, drop) {
 #' @export
 dim.BEDMatrix <- function (x) {
   return(c(x@n, x@p))
+}
+
+#' @export
+dimnames.BEDMatrix <- function (x) {
+  return(x@dimnames)
+}
+
+#' @export
+`dimnames<-.BEDMatrix` <- function (x, value) {
+  d <- dim(x)
+  v1 <- value[[1]]
+  v2 <- value[[2]]
+  if (!is.list(value) || length(value) != 2 ||
+      !(is.null(v1) || length(v1) == d[1]) ||
+      !(is.null(v2) || length(v2) == d[2])) {
+    stop('invalid dimnames')
+  }
+  x@dimnames <- lapply(value, function (v) {
+    if (!is.null(v)) {
+      as.character(v)
+    }
+  })
+  return(x)
 }
