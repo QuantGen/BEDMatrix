96c57e245dcc73214f9eb714835fcd5c096524b5
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-06-03 17:13:23 -0500

    Check if given dimensions match the file.

--- src/subsetBED.cpp
+++ src/subsetBED.cpp
@@ -49,37 +49,45 @@ Rcpp::IntegerMatrix subsetBED(Rcpp::List x, Rcpp::IntegerVector i, Rcpp::Integer
     if (header[0] == '\x6C' && header[1] == '\x1B') {
       // Check individual-major mode
       if (header[2] == '\x01') {
-        // Iterate over rows
-        for (int idx_i = 0; idx_i < size_i; idx_i++) {
-          // Iterate over columns
-          for (int idx_j = 0; idx_j < size_j; idx_j++) {
-            // Reduce two-dimensional index to one-dimensional index
-            int whichPos = (i[idx_i] * p) + j[idx_j];
-            // Every byte encodes 4 genotypes, find the one of interest
-            int whichByte = std::floor(whichPos / 4);
-            // Find genotype in byte
-            int whichGenotype = (whichPos % 4) * 2;
-            // Read in the whole byte
-            infile.seekg(whichByte + 3);
-            char *genotypes = new char[1];
-            infile.read(genotypes, 1);
-            // Remove the other genotypes
-            int genotype = genotypes[0] >> whichGenotype & 3;
-            // Remap genotype value
-            int mapping;
-            if (genotype == 0) {
-              mapping = 0; // homozygous AA
-            } else if (genotype == 3) {
-              mapping = 2; // homozygous BB
-            } else if (genotype == 1) {
-              mapping = 1; // heterozygous AB
-            } else if (genotype == 2) {
-              mapping = NA_INTEGER; // missing
-            } else {
-              Rcpp::stop("Invalid genotype.");
+        // Get number of bytes
+        infile.seekg(0, infile.end);
+        int num_bytes = infile.tellg();
+        // Check if given dimensions match the file
+        if (n * p <= (num_bytes - 3) * 4) {
+          // Iterate over rows
+          for (int idx_i = 0; idx_i < size_i; idx_i++) {
+            // Iterate over columns
+            for (int idx_j = 0; idx_j < size_j; idx_j++) {
+              // Reduce two-dimensional index to one-dimensional index
+              int whichPos = (i[idx_i] * p) + j[idx_j];
+              // Every byte encodes 4 genotypes, find the one of interest
+              int whichByte = std::floor(whichPos / 4);
+              // Find genotype in byte
+              int whichGenotype = (whichPos % 4) * 2;
+              // Read in the whole byte
+              infile.seekg(whichByte + 3);
+              char *genotypes = new char[1];
+              infile.read(genotypes, 1);
+              // Remove the other genotypes
+              int genotype = genotypes[0] >> whichGenotype & 3;
+              // Remap genotype value
+              int mapping;
+              if (genotype == 0) {
+                mapping = 0; // homozygous AA
+              } else if (genotype == 3) {
+                mapping = 2; // homozygous BB
+              } else if (genotype == 1) {
+                mapping = 1; // heterozygous AB
+              } else if (genotype == 2) {
+                mapping = NA_INTEGER; // missing
+              } else {
+                Rcpp::stop("Invalid genotype.");
+              }
+              out(idx_i, idx_j) = mapping;
             }
-            out(idx_i, idx_j) = mapping;
           }
+        } else {
+          Rcpp::stop("n or p does not match the dimensions of the file.");
         }
       } else {
         Rcpp::stop("Individual major mode is not supported.");
