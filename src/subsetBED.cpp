33a314ea15ce423daaadb91f1d7fbd9f291d40b8
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-06-04 14:56:41 -0500

    Improve comments.

--- src/subsetBED.cpp
+++ src/subsetBED.cpp
@@ -13,17 +13,17 @@ Rcpp::IntegerMatrix subsetBED(Rcpp::List x, Rcpp::IntegerVector i, Rcpp::Integer
   std::string path = x.attr("path");
   int n = x.attr("n");
   int p = x.attr("p");
-  // Check if indexes are out of bounds
+  // Check if indexes are out of bounds.
   if (Rcpp::is_true(Rcpp::any(i > n)) || Rcpp::is_true(Rcpp::any(j > p))) {
     Rcpp::stop("Invalid dimensions.");
   }
   // Convert from 1-index to 0-index.
   i = i - 1;
   j = j - 1;
-  // Keep sizes of i and j
+  // Keep sizes of i and j.
   int size_i = i.size();
   int size_j = j.size();
-  // Reserve output matrix
+  // Reserve output matrix.
   Rcpp::IntegerMatrix out (size_i, size_j);
   // Preserve dimnames.
   Rcpp::List out_dimnames = Rcpp::List::create(
@@ -40,37 +40,38 @@ Rcpp::IntegerMatrix subsetBED(Rcpp::List x, Rcpp::IntegerVector i, Rcpp::Integer
     out_dimnames[1] = Rcpp::CharacterVector(in_colnames)[j];
   }
   out.attr("dimnames") = out_dimnames;
-  // Open BED file
+  // Open BED file.
   std::ifstream infile (path.c_str(), std::ios::binary);
   if (infile) {
     char *header = new char[3];
     infile.read(header, 3);
-    // Check magic number
+    // Check magic number.
     if (header[0] == '\x6C' && header[1] == '\x1B') {
-      // Check individual-major mode
+      // Check individual-major mode.
       if (header[2] == '\x01') {
-        // Get number of bytes
+        // Get number of bytes.
         infile.seekg(0, infile.end);
         int num_bytes = infile.tellg();
-        // Check if given dimensions match the file
+        // Check if given dimensions match the file.
         if (n * p <= (num_bytes - 3) * 4) {
-          // Iterate over rows
+          // Iterate over rows indexes.
           for (int idx_i = 0; idx_i < size_i; idx_i++) {
-            // Iterate over columns
+            // Iterate over columns indexes.
             for (int idx_j = 0; idx_j < size_j; idx_j++) {
-              // Reduce two-dimensional index to one-dimensional index
+              // Reduce two-dimensional index to one-dimensional index with the mode.
               int whichPos = (i[idx_i] * p) + j[idx_j];
-              // Every byte encodes 4 genotypes, find the one of interest
+              // Every byte encodes 4 genotypes, find the one of interest.
               int whichByte = std::floor(whichPos / 4);
-              // Find genotype in byte
+              // Find genotype in byte.
               int whichGenotype = (whichPos % 4) * 2;
-              // Read in the whole byte
+              // Read in the whole byte.
               infile.seekg(whichByte + 3);
               char *genotypes = new char[1];
               infile.read(genotypes, 1);
-              // Remove the other genotypes
+              // Remove the other genotypes by shifting the genotype of interest
+              // to the end of the byte and masking with 00000011.
               int genotype = genotypes[0] >> whichGenotype & 3;
-              // Remap genotype value
+              // Remap genotype value.
               int mapping;
               if (genotype == 0) {
                 mapping = 0; // homozygous AA
