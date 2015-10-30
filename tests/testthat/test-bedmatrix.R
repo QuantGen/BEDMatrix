708d1e7ee5715ad8e6d5a418dac112925fd5b0cf
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-10-30 14:37:00 -0400

    Explicitly test against error messages.

--- tests/testthat/test-bedmatrix.R
+++ tests/testthat/test-bedmatrix.R
@@ -20,43 +20,43 @@ examplePath <- system.file("extdata", "example.bed", package = "BEDMatrix")
 standalonePath <- system.file("extdata", "standalone.bed", package = "BEDMatrix")
 
 test_that("it throws an error if file does not exist", {
-    expect_error(BEDMatrix("NOT_FOUND"))
+    expect_error(BEDMatrix("NOT_FOUND"), "File not found\\.")
 })
 
 test_that("it throws an error if file is not a BED file", {
-    expect_error(BEDMatrix(system.file("extdata", "example.raw", package = "BEDMatrix")))
+    expect_error(BEDMatrix(system.file("extdata", "example.raw", package = "BEDMatrix")), "File is not a binary PED file\\.")
 })
 
 test_that("it determines n from FAM file", {
     bed <- BEDMatrix(path = examplePath)
     expect_equal(nrow(bed), 6)
-    expect_message(BEDMatrix(path = examplePath))
+    expect_message(BEDMatrix(path = examplePath), "Extracting number of individuals and rownames from FAM file\\.\\.\\.")
 })
 
 test_that("it throws an error if FAM file is not found and n is not given", {
-    expect_error(BEDMatrix(path = standalonePath))
+    expect_error(BEDMatrix(path = standalonePath), "FAM file of same name not found\\. Provide number of individuals \\(n\\)\\.")
 })
 
 test_that("it determines rownames from FAM file", {
     bed <- BEDMatrix(path = examplePath)
     expect_equal(rownames(bed), c("1_1", "1_2", "1_3", "2_1", "2_2", "2_3"))
-    expect_message(BEDMatrix(path = examplePath))
+    expect_message(BEDMatrix(path = examplePath), "Extracting number of individuals and rownames from FAM file\\.\\.\\.")
 })
 
 test_that("it determines p from BIM file", {
     bed <- BEDMatrix(path = examplePath)
     expect_equal(ncol(bed), 3)
-    expect_message(BEDMatrix(path = examplePath))
+    expect_message(BEDMatrix(path = examplePath), "Extracting number of markers and colnames from BIM file\\.\\.\\.")
 })
 
 test_that("it throws an error if BIM file is not found and p is not given", {
-    expect_error(BEDMatrix(path = standalonePath))
+    expect_error(BEDMatrix(path = standalonePath), "FAM file of same name not found\\. Provide number of individuals \\(n\\)\\.")
 })
 
 test_that("it determines colnames from BIM file", {
     bed <- BEDMatrix(path = examplePath)
     expect_equal(colnames(bed), c("snp1", "snp2", "snp3"))
-    expect_message(BEDMatrix(path = examplePath))
+    expect_message(BEDMatrix(path = examplePath), "Extracting number of markers and colnames from BIM file\\.\\.\\.")
 })
 
 test_that("it accepts n and p if FAM or BIM file are present", {
@@ -70,7 +70,7 @@ test_that("it accepts n and p if FAM or BIM file is not found", {
 })
 
 test_that("it throws an error if dimensions are wrong", {
-    expect_error(BEDMatrix(path = examplePath, n = 10, p = 5))
+    expect_error(BEDMatrix(path = examplePath, n = 10, p = 5), "n or p does not match the dimensions of the file\\.")
 })
 
 test_that("subsetting", {
