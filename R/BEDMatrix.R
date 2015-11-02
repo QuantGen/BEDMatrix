473e10753eda080ea49a82e2cb08be1bb6e3bed3
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-11-02 18:01:54 -0500

    Print NIH grant numbers when attaching the package.

--- R/BEDMatrix.R
+++ R/BEDMatrix.R
@@ -1,6 +1,15 @@
 #' @useDynLib BEDMatrix
 #' @import methods Rcpp
+NULL
+
+
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("Support provided by the National Institutes of Health (Grant: R01GM101219, R01GM099992).")
+}
+
+
 loadModule("mod_BEDMatrix", TRUE)
+
 
 # Delimiters used in PED files.
 delims <- "[ \t]"
