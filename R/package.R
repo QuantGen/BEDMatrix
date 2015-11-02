0cc93f413c00e9e09d7ba5b16e8e318bd09947f1
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-11-02 18:14:07 -0500

    Add package documentation.

--- R/package.R
+++ R/package.R
@@ -1,3 +1,13 @@
+#' BEDMatrix: A Wrapper for Binary PED Files
+#' 
+#' The BEDMatrix package provides a wrapper around binary PED files (so-called
+#' BED files) that behaves just like a regular R matrix, but retrieves
+#' genotypes on demand without loading the entire BED file into memory. The
+#' goal is to support huge PED files, and to save time on initially reading in
+#' PED files into the R environment.
+#' 
+#' @docType package
+#' @name BEDMatrix
 #' @useDynLib BEDMatrix
 #' @import methods Rcpp
 NULL
