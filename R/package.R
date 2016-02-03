5c9e224edef630a07ef95426a1032b34f2274e41
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2016-02-03 15:26:33 -0500

    Remove startup message for now.

--- R/package.R
+++ R/package.R
@@ -18,12 +18,6 @@
 NULL
 
 
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("The BEDMatrix package was supported by the National Institutes of Health (Grant: R01GM101219, R01GM099992).")
-    packageStartupMessage()
-}
-
-
 loadModule("mod_BEDMatrix", TRUE)
 
 
