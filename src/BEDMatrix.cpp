c7fc234a0ae7303d95bbb7020fef15d62047e30f
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2019-02-28 14:20:24 -0500

    Rename nrow to num_samples and ncol to num_variants

--- src/BEDMatrix.cpp
+++ src/BEDMatrix.cpp
@@ -24,11 +24,11 @@ class BEDMatrix {
         boost::interprocess::file_mapping file;
         boost::interprocess::mapped_region file_region;
         uint8_t* file_data;
-        std::size_t nrow;
-        std::size_t ncol;
+        std::size_t num_samples;
+        std::size_t num_variants;
 };
 
-BEDMatrix::BEDMatrix(std::string path, std::size_t n, std::size_t p) : nrow(n), ncol(p) {
+BEDMatrix::BEDMatrix(std::string path, std::size_t n, std::size_t p) : num_samples(n), num_variants(p) {
     try {
         this->file = boost::interprocess::file_mapping(path.c_str(), boost::interprocess::read_only);
     } catch(const boost::interprocess::interprocess_exception& e) {
@@ -51,7 +51,7 @@ BEDMatrix::BEDMatrix(std::string path, std::size_t n, std::size_t p) : nrow(n), 
     // Get number of bytes
     const std::size_t num_bytes = this->file_region.get_size();
     // Check if given dimensions match the file
-    if ((this->ncol * ceil((double) this->nrow / PLINK_BED_GENOTYPES_PER_BYTE)) != (num_bytes - PLINK_BED_HEADER_LENGTH)) {
+    if ((this->num_variants * ceil((double) this->num_samples / PLINK_BED_GENOTYPES_PER_BYTE)) != (num_bytes - PLINK_BED_HEADER_LENGTH)) {
         throw std::runtime_error("n or p does not match the dimensions of the file.");
     }
 }
@@ -59,7 +59,7 @@ BEDMatrix::BEDMatrix(std::string path, std::size_t n, std::size_t p) : nrow(n), 
 int BEDMatrix::get_genotype(std::size_t i, std::size_t j) {
     // Each byte encodes 4 genotypes; adjust indices
     std::size_t i_bytes = i / PLINK_BED_GENOTYPES_PER_BYTE;
-    std::size_t n_bytes = this->nrow / PLINK_BED_GENOTYPES_PER_BYTE + (this->nrow % PLINK_BED_GENOTYPES_PER_BYTE != 0); // fast ceil for int
+    std::size_t n_bytes = this->num_samples / PLINK_BED_GENOTYPES_PER_BYTE + (this->num_samples % PLINK_BED_GENOTYPES_PER_BYTE != 0); // fast ceil for int
     std::size_t i_genotypes = 2 * (i - i_bytes * PLINK_BED_GENOTYPES_PER_BYTE);
     // Load byte from map
     uint8_t genotypes = this->file_data[PLINK_BED_HEADER_LENGTH + (j * n_bytes + i_bytes)];
@@ -89,13 +89,13 @@ Rcpp::IntegerVector BEDMatrix::extract_vector(Rcpp::IntegerVector i) {
     // Reserve output vector
     Rcpp::IntegerVector out(size_i);
     // Get bounds
-    std::size_t bounds = this->nrow * this->ncol;
+    std::size_t bounds = this->num_samples * this->num_variants;
     // Iterate over indexes
     for (std::size_t idx_i = 0; idx_i < size_i; idx_i++) {
         if (Rcpp::IntegerVector::is_na(i0[idx_i]) || static_cast<std::size_t>(i0[idx_i]) >= bounds) {
             out(idx_i) = NA_INTEGER;
         } else {
-            out(idx_i) = this->get_genotype(i0[idx_i] % this->nrow, i0[idx_i] / this->nrow);
+            out(idx_i) = this->get_genotype(i0[idx_i] % this->num_samples, i0[idx_i] / this->num_samples);
         }
     }
     return out;
@@ -103,7 +103,7 @@ Rcpp::IntegerVector BEDMatrix::extract_vector(Rcpp::IntegerVector i) {
 
 Rcpp::IntegerMatrix BEDMatrix::extract_matrix(Rcpp::IntegerVector i, Rcpp::IntegerVector j) {
     // Check if indexes are out of bounds
-    if (Rcpp::is_true(Rcpp::any(i > this->nrow)) || Rcpp::is_true(Rcpp::any(j > this->ncol))) {
+    if (Rcpp::is_true(Rcpp::any(i > this->num_samples)) || Rcpp::is_true(Rcpp::any(j > this->num_variants))) {
         throw std::runtime_error("subscript out of bounds");
     }
     // Convert from 1-index to 0-index
