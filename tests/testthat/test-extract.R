9fd087c66053144a301b4d1fd6b4a12eeb1faf96
Alexander Grueneberg <git@agrueneberg.info>
2020-05-16 07:43:39 -0400

    Pass test environment via local argument

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -1,7 +1,10 @@
 # Source extraction tests
-CROCHET_EXTRACT_ENV <- new.env()
-CROCHET_EXTRACT_ENV$COMPARE_OBJECT <- raw
-CROCHET_EXTRACT_ENV$CUSTOM_OBJECT <- bed
-CROCHET_EXTRACT_ENV$OUT_OF_BOUNDS_INT <- length(CROCHET_EXTRACT_ENV$CUSTOM_OBJECT) + 1
-CROCHET_EXTRACT_ENV$OUT_OF_BOUNDS_CHAR <- "snp1000_U"
-source(system.file("test-suite", "crochet-extract.R", package = "crochet"), local = TRUE)
+extractionTests <- new.env()
+extractionTests$COMPARE_OBJECT <- raw
+extractionTests$CUSTOM_OBJECT <- bed
+extractionTests$OUT_OF_BOUNDS_INT <- length(extractionTests$CUSTOM_OBJECT) + 1
+extractionTests$OUT_OF_BOUNDS_CHAR <- "snp1000_U"
+source(
+    file = system.file("test-suite", "crochet-extract.R", package = "crochet"),
+    local = extractionTests
+)
